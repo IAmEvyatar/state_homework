97c2eb2026d13e9847a0d633556bbb41d4d6d2bc
IAmEvyatar <evyatarsilberschatz@gmail.com>
2019-03-05 13:51:21 +0200

    adds a changeBgColor method to state and ties it to html button

--- 001.js
+++ 001.js
@@ -1,8 +1,12 @@
 const state = {
     formHandler: function(e){
-        console.log(e.currentTarget.colorInput.value)
+        state.bgColor = e.currentTarget.colorInput.value;
         e.preventDefault()
         return false;
     },
-}
+    changeBgColor: function(){
+        document.body.style.backgroundColor = state.bgColor;
+    }
+};
 document.getElementById("bg-color-form").onsubmit = state.formHandler;
+document.getElementById("change-background-button").onclick = state.changeBgColor;
